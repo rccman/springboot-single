9ba5a96c7d5f7e45dbe5b4f69c9cd0f7ec988b25
rencc <rccman@163.com>
2019-05-08 23:20:32 +0800

    枚举

---
+++ test-service/src/main/java/com/rcc/test/test/base/enums/SimpleEnum.java
@@ -0,0 +1,16 @@
+package com.rcc.test.test.base.enums;
+
+enum SimpleEnum {
+        SPRING,  
+        SUMMER,  
+        AUTUMN,  
+        WINTER;
+
+    public static void main(String[] args) {
+        System.out.println(SimpleEnum.SPRING);
+        System.out.println(SimpleEnum.SUMMER);
+        System.out.println(SimpleEnum.AUTUMN);
+        System.out.println(SimpleEnum.WINTER);
+    }
+
+}
